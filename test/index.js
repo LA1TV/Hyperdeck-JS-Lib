4dfc6cb07e7f8d3f8811d453181e94ed0374c7eb
Tom Jenkinson <tjcode@outlook.com>
2016-04-09 16:25:25 +0100

    Add test to check for "HyperdeckCore"

--- test/index.js
+++ test/index.js
@@ -4,4 +4,8 @@ describe('index', function() {
   it('should provide the Hyperdeck class', function() {
     index.Hyperdeck.should.be.ok;
   });
+
+  it('should provide the HyperdeckCore class', function() {
+    index.HyperdeckCore.should.be.ok;
+  });
 });

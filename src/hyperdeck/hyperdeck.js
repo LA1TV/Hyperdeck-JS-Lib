19aef9fb2aa3613961aa82d20e25b75cce051222
Thomas Kaltz III <tomkaltz@gmail.com>
2018-06-11 15:42:23 -0400

    revert first fix then use regular bind

--- src/hyperdeck/hyperdeck.js
+++ src/hyperdeck/hyperdeck.js
@@ -62,13 +62,13 @@ var Hyperdeck = function(config) {
   };
 
   this.format = function(format){
-    return this.makeRequest('format: prepare: ' + format).then((response)=>{
+    return this.makeRequest('format: prepare: ' + format).then(function(response){
       if (response.code !== 216 || response.text !== 'format ready' || !response.rawData) {
         throw new Error('Unexpected response.');
       }
       var token = response.rawData;
       return this.makeRequest('format: confirm: ' + token);
-    });
+    }.bind(this));
   };
 };
 
